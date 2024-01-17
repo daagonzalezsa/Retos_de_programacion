a67591228979230082722af37aecafdd71a895ab
Daniel Gonzalez <daagonzalezsa@unal.edu.co>
2024-01-17 09:15:51 -0500

    Done the 4th challenge and generate the expected value

--- Logica/Reto_4.py
+++ Logica/Reto_4.py
@@ -2,3 +2,13 @@
 # Escribe un programa que se encargue de comprobar si un número es o no primo.
 # Hecho esto, imprime los números primos entre 1 y 100.
 
+Primes = []
+for i in range(1, 101):
+    divisors = []
+    for j in range(1,i+1):
+        if (i % j==0):
+            divisors.append(j)
+    if len(divisors) <= 2:
+        Primes.append(i)
+
+print(Primes)
