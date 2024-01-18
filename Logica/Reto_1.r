247eb75fac12ebabf782cf58b0c1df1dc91176e2
Daniel Gonzalez <daagonzalezsa@unal.edu.co>
2024-01-17 19:25:53 -0500

    Correct the identation of the code

--- Logica/Reto_1.r
+++ Logica/Reto_1.r
@@ -1,12 +1,11 @@
-for(i in 1:100) {
-    if (i%%3 == 0 & i %% 5==0) {
-        print("fizzbuzz")
-    } else if(i%%3 == 0) {
-        print("fizz")
-    } else if (i %% 5 == 0) {
-       print("buzz")
-    } else {
-       print(i)
-    }
+for (i in 1:100) {
+  if (i %% 3 == 0 && i %% 5 == 0) { # nolint
+    print("fizzbuzz")
+  } else if (i %% 3 == 0) {
+    print("fizz")
+  } else if (i %% 5 == 0) {
+    print("buzz")
+  } else {
+    print(i)
+  }
 }
-
